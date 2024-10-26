8bc057d2604f5fde63e1a06351f28dfac92c9f13
railmedia <adrian@railmedia.ro>
2024-10-26 13:33:37 +0200

    Languages edits

---
+++ bin/generate-language-files.sh
@@ -0,0 +1,25 @@
+rm -rf ../languages/order-tip-woo-de_DE.po
+rm -rf ../languages/order-tip-woo-de_DE.mo
+rm -rf ../languages/order-tip-woo-de_DE-formal.po
+rm -rf ../languages/order-tip-woo-de_DE-formal.mo
+rm -rf ../languages/order-tip-woo-de_DE-formal.po
+rm -rf ../languages/order-tip-woo-de_DE-formal.mo
+
+cp ../languages/order-tip-woo-de_CH.po ../languages/order-tip-woo-de_DE.po
+cp ../languages/order-tip-woo-de_CH.mo ../languages/order-tip-woo-de_DE.mo
+cp ../languages/order-tip-woo-de_CH.po ../languages/order-tip-woo-de_DE-formal.po
+cp ../languages/order-tip-woo-de_CH.mo ../languages/order-tip-woo-de_DE-formal.mo
+cp ../languages/order-tip-woo-de_CH.po ../languages/order-tip-woo-de_CH-formal.po
+cp ../languages/order-tip-woo-de_CH.mo ../languages/order-tip-woo-de_CH-formal.mo
+
+rm -rf ../languages/order-tip-woo-fr_FR.po
+rm -rf ../languages/order-tip-woo-fr_FR.mo
+
+cp ../languages/order-tip-woo-fr_CH.po ../languages/order-tip-woo-fr_FR.po
+cp ../languages/order-tip-woo-fr_CH.mo ../languages/order-tip-woo-fr_FR.mo
+
+rm -rf ../languages/order-tip-woo-it_IT.po
+rm -rf ../languages/order-tip-woo-it_IT.mo
+
+cp ../languages/order-tip-woo-it_CH.po ../languages/order-tip-woo-it_IT.po
+cp ../languages/order-tip-woo-it_CH.mo ../languages/order-tip-woo-it_IT.mo
